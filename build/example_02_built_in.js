f96f25e01b7ce73018293308dee123433670d07d
John Baker <phibonacci85@gmail.com>
2018-05-03 07:15:54 -0500

    built in

--- build/example_02_built_in.js
+++ build/example_02_built_in.js
@@ -8,12 +8,30 @@ var _util = require('./lib/util');
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-// Rx.Observable.interval(500)
-//   .take(5)
-//   .subscribe(createSubscriber('interval'));
-//
-// Rx.Observable.timer(3000, 500)
-//   .take(3)
-//   .subscribe(createSubscriber('timer'));
+Rx.Observable.interval(500).take(5).subscribe((0, _util.createSubscriber)('interval'));
 
-Rx.Observable.of('Hello World', 42, 'whoa').subscribe((0, _util.createSubscriber)('of'));
+Rx.Observable.timer(3000, 500).take(3).subscribe((0, _util.createSubscriber)('timer'));
+
+Rx.Observable.of(['Hello World', 56, 'whoa']).subscribe((0, _util.createSubscriber)('of'));
+
+Rx.Observable.from(['Hello World', 42, 'whoa']).subscribe((0, _util.createSubscriber)('from'));
+
+Rx.Observable.from([new Error('Hey')]).subscribe((0, _util.createSubscriber)('fromError'));
+
+Rx.Observable.throwError(928).subscribe((0, _util.createSubscriber)('error'));
+
+Rx.Observable.empty().subscribe((0, _util.createSubscriber)('empty'));
+
+var sideEffect = 0;
+var defer$ = Rx.Observable.defer(function () {
+  sideEffect++;
+  return Rx.Observable.of(sideEffect);
+});
+
+defer$.subscribe((0, _util.createSubscriber)('defer$.one'));
+defer$.subscribe((0, _util.createSubscriber)('defer$.two'));
+defer$.subscribe((0, _util.createSubscriber)('defer$.three'));
+
+Rx.Observable.never().subscribe((0, _util.createSubscriber)('never'));
+
+Rx.Observable.range(2, 5).subscribe((0, _util.createSubscriber)('range'));
--- src-server/example_02_built_in.js
+++ src-server/example_02_built_in.js
@@ -1,13 +1,41 @@
 import * as Rx from 'rxjs/Rx';
 import { createSubscriber } from './lib/util';
 
-// Rx.Observable.interval(500)
-//   .take(5)
-//   .subscribe(createSubscriber('interval'));
-//
-// Rx.Observable.timer(3000, 500)
-//   .take(3)
-//   .subscribe(createSubscriber('timer'));
+Rx.Observable.interval(500)
+  .take(5)
+  .subscribe(createSubscriber('interval'));
 
-Rx.Observable.of('Hello World', 42, 'whoa')
+Rx.Observable.timer(3000, 500)
+  .take(3)
+  .subscribe(createSubscriber('timer'));
+
+Rx.Observable.of(['Hello World', 56, 'whoa'])
   .subscribe(createSubscriber('of'));
+
+Rx.Observable.from(['Hello World', 42, 'whoa'])
+  .subscribe(createSubscriber('from'));
+
+Rx.Observable.from([new Error('Hey')])
+  .subscribe(createSubscriber('fromError'));
+
+Rx.Observable.throwError(928)
+  .subscribe(createSubscriber('error'));
+
+Rx.Observable.empty()
+  .subscribe(createSubscriber('empty'));
+
+let sideEffect = 0;
+const defer$ = Rx.Observable.defer(() => {
+  sideEffect++;
+  return Rx.Observable.of(sideEffect);
+});
+
+defer$.subscribe(createSubscriber('defer$.one'));
+defer$.subscribe(createSubscriber('defer$.two'));
+defer$.subscribe(createSubscriber('defer$.three'));
+
+Rx.Observable.never()
+  .subscribe(createSubscriber('never'));
+
+Rx.Observable.range(2, 5)
+  .subscribe(createSubscriber('range'));
